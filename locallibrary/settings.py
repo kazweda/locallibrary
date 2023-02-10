fa9499378ae5fbb71562dd484bda276ef0b6b2a6
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-02-10 16:32:37 +0900

    #8 Testing in templates まで。

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from .settings_local import *
+import os # needed by code below
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,7 +42,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,3 +109,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login (Default redirects to /accounts/profile/)
+LOGIN_REDIRECT_URL = '/'
