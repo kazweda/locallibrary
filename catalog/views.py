e741acc3beced38cec8a3acb902a6f01e892266f
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-02-10 11:21:08 +0900

    fix session-based visit-counter #7

--- catalog/views.py
+++ catalog/views.py
@@ -4,9 +4,6 @@ from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 
 def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
 
@@ -16,14 +13,17 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
-    # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
 from django.views import generic
