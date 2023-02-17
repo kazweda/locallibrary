e4545ddc200bb2807cbe3016288c5667f210f46d
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-02-17 14:43:02 +0900

    fix #9

--- catalog/views.py
+++ catalog/views.py
@@ -79,7 +79,8 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-from catalog.forms import RenewBookForm
+# from catalog.forms import RenewBookForm
+from catalog.forms import RenewBookModelForm
 
 @login_required
 @permission_required('catalog.can_mark_returned', raise_exception=True)
@@ -113,3 +114,21 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from catalog.models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
