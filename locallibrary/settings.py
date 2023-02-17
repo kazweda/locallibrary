4685ded2dc65f414b9c6650b8847a8146f405e1f
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-02-17 16:05:49 +0900

    #10 テストの実行とFAILED (failures=1)まで。

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -99,10 +99,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'  #. os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
